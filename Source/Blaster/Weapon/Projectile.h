00f78fa49688ad4371898cb8fd08962e0de520da
fir3e <fir3ez@gmail.com>
2024-01-21 16:10:56 +0100

    78. Projectile Hit Events

--- Source/Blaster/Weapon/Projectile.h
+++ Source/Blaster/Weapon/Projectile.h
@@ -14,9 +14,12 @@ class BLASTER_API AProjectile : public AActor
 public:	
 	AProjectile();
 	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 private:
 	UPROPERTY(EditAnywhere)
 	class UBoxComponent* CollisionBox;
@@ -29,6 +32,11 @@ private:
 
 	class UParticleSystemComponent* TracerComponent;
 
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ImpactSound;
 
 public:
 	
