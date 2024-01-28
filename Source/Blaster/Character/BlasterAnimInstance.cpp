5e91096085d790c26e2102f9c11c50187b6c984d
fir3e <fir3ez@gmail.com>
2024-01-28 03:55:07 +0100

    86. Zooming While Aim

--- Source/Blaster/Character/BlasterAnimInstance.cpp
+++ Source/Blaster/Character/BlasterAnimInstance.cpp
@@ -74,7 +74,7 @@ void UBlasterAnimInstance::NativeUpdateAnimation(float DeltaTime)
 		if (BlasterCharacter->IsLocallyControlled())
 		{
 			bLocallyControlled = true;
-			FTransform RightHandTransform = EquippedWeapon->GetWeaponMesh()->GetSocketTransform(FName("Hand_R"), ERelativeTransformSpace::RTS_World);
+			FTransform RightHandTransform = BlasterCharacter->GetMesh()->GetSocketTransform(FName("Hand_R"), ERelativeTransformSpace::RTS_World);
 			RightHandRotation = UKismetMathLibrary::FindLookAtRotation(RightHandTransform.GetLocation(), RightHandTransform.GetLocation() + (RightHandTransform.GetLocation() - BlasterCharacter->GetHitTarget()));
 		}
 	}
