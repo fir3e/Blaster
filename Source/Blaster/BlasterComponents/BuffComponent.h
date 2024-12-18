cb7fc85dc0f45bf0fd46b77ef0794f6dbc0bc917
fir3e <fir3ez@gmail.com>
2024-12-18 17:34:58 +0100

    164. Speed Buffs

--- Source/Blaster/BlasterComponents/BuffComponent.h
+++ Source/Blaster/BlasterComponents/BuffComponent.h
@@ -15,7 +15,10 @@ class BLASTER_API UBuffComponent : public UActorComponent
 public:	
 	UBuffComponent();
 	friend class ABlasterCharacter;
+
 	void Heal(float HealAmount, float HealingTime);
+	void BuffSpeed(float BuffBaseSpeed, float BuffCrouchSpeed, float BuffTime);
+	void SetInitialSpeeds(float BaseSpeed, float CrouchSpeed);
 protected:
 	virtual void BeginPlay() override;
 	void HealRampUp(float DeltaTime);
@@ -24,9 +27,25 @@ private:
 	UPROPERTY()
 	class ABlasterCharacter* Character;
 
+	/**
+	* Heal buff
+	*/
+
 	bool bHealing = false;
 	float HealingRate = 0.f;
 	float AmountToHeal = 0.f;
+
+	/**
+	* Speed buff
+	*/
+
+	FTimerHandle SpeedBuffTimer;
+	void ResetSpeeds();
+	float InitialBaseSpeed;
+	float InitialCrouchSpeed;
+
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastSpeedBuff(float BaseSpeed, float CrouchSpeed);
 
 public:	
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
