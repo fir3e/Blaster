71adcb72785ed628dc968704fc2a17de13b7830f
fir3e <fir3ez@gmail.com>
2025-02-03 01:28:49 +0100

    194. Interp Between Frames

--- Source/Blaster/BlasterComponents/LagCompensationComponent.h
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.h
@@ -50,6 +50,7 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	void SaveFramePackage(FFramePackage& Package);
+	FFramePackage InterpBetweenFrames(const FFramePackage& OlderFrame, const FFramePackage& YoungerFrame, float HitTime);
 
 	UPROPERTY()
 	ABlasterCharacter* Character;
