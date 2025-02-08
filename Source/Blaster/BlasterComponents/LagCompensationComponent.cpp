d653d0c01a907144b0791faace412608bd6c86ff
fir3e <fir3ez@gmail.com>
2025-02-08 15:59:29 +0100

    200. Requesting a Shotgun Hit

--- Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -339,6 +339,7 @@ FFramePackage ULagCompensationComponent::GetFrameToCheck(ABlasterCharacter* HitC
 		// Interpolate between Younger and Older
 		FrameToCheck = InterpBetweenFrames(Older->GetValue(), Younger->GetValue(), HitTime);
 	}
+	FrameToCheck.Character = HitCharacter;
 	return FFramePackage();
 }
 
