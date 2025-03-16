ab029a0047feb9a71b810ab78e2d17ea38f9408b
fir3e <fir3ez@gmail.com>
2025-03-16 14:48:45 +0100

    219. Head Shots

--- Source/Blaster/Weapon/Weapon.h
+++ Source/Blaster/Weapon/Weapon.h
@@ -134,6 +134,9 @@ protected:
 	float SphereRadius = 75.f;
 
 	UPROPERTY(EditAnywhere)
+	float HeadShotDamage = 40.f;
+
+	UPROPERTY(EditAnywhere)
 	float Damage = 20.f;
 
 	UPROPERTY(Replicated, EditAnywhere)
@@ -200,4 +203,5 @@ public:
 	FORCEINLINE int32 GetAmmo() const { return Ammo; }
 	FORCEINLINE int32 GetMagCapacity() const { return MagCapacity; }
 	FORCEINLINE float GetDamage() const { return Damage; }
+	FORCEINLINE float GetHeadShotDamage() const { return HeadShotDamage; }
 };
