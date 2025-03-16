479f4e6f8413d1d632abd112feaebcdd77177199
fir3e <fir3ez@gmail.com>
2025-03-17 00:42:31 +0100

    223. Teams

--- Source/Blaster/PlayerState/BlasterPlayerState.h
+++ Source/Blaster/PlayerState/BlasterPlayerState.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "Blaster/BlasterTypes/Team.h"
 #include "BlasterPlayerState.generated.h"
 
 /**
@@ -36,7 +37,12 @@ private:
 	UPROPERTY(ReplicatedUsing = OnRep_Defeats)
 	int32 Defeats;
 
+	UPROPERTY(Replicated)
+	ETeam Team = ETeam::ET_NoTeam;
+
 public:
 	FORCEINLINE int32 GetDefeats() const { return Defeats; }
 	FORCEINLINE void SetDefeats(int32 NewDefeats) { Defeats = NewDefeats; }
+	FORCEINLINE ETeam GetTeam() const { return Team; }
+	FORCEINLINE void SetTeam(ETeam TeamToSet) { Team = TeamToSet; }
 };
